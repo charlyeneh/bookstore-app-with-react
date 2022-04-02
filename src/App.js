cd7af12268c5c0e64629241e39cba4ecc3795ff3
Charlyeneh <charleschukwunweikeeneh@gmail.com>
2022-04-02 20:45:18 +0100

    Add components and UI logic

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Books from './components/Books';
+import Catagories from './components/Catagories';
+import Navbar from './components/Navbar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Books />} />
+        <Route path="bookstore" element={<Books />} />
+        <Route path="/Catagories" element={<Catagories />} />
+      </Routes>
+    </Router>
+  </div>
+);
 
 export default App;
